718f25d7d98db67895a86c49c0be458d21779759
KAZUMA Ukyo <yaakaito@gmail.com>
2013-07-31 12:15:50 +0900

    Revert proxy

--- src/util/proxy.ts
+++ src/util/proxy.ts
@@ -1,9 +1,9 @@
-function proxy(fn : (...args: any[]) => any, context: any): any {
+function proxy(fn : (...args: any[]) => any, context: any, ...args: any[]): any {
     if (!isFunction(fn)) {
         return undefined;
     }
 
     return function(){
-        return fn.apply( context || this, arguments);
+        return fn.apply( context || this, args);
     }
 }
